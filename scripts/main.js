8f4394779b5b775ee078037acb782759e4bd327f
Gaston Nan <gaston.nan@zedplan.com>
2015-02-07 16:19:12 -0300

    moviles

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,11 @@
 $(function(){
     
+    var isMobile = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+    
+    if( isMobile ){
+        $('#cr-stage').remove();
+        $('#instructions').remove();
+        $('#noMobile').show();
+    }
     
 });
